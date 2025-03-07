b84dd7901f11eae5ed64c8770ca3a93e819bff4f
Altonot <altin.bakiu@ucalgary.ca>
2025-03-07 11:56:33 -0700

    ex1

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const date = new Date()
+  let currentYear = date.getFullYear()
+  let isLoggedIn = true;
+
+  function checkLogin(isLoggedIn){
+    return isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+      <p>{currentYear}</p>
+      {checkLogin(isLoggedIn)}
     </div>
   );
 }
